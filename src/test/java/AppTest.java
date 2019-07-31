92c85648c86222222b827f010f5cdbbd90768184
Yuliia Shevchenko <yuliiashevchenko@Aslanbeks-MacBook-Pro.local>
2019-07-30 21:32:23 -0400

    added more prints

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,10 @@
 public class AppTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
